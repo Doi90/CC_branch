71a1c037c902f084300cacf40b00a622d62544e2
Elliot Gould <elliot.gould@unimelb.edu.au>
2021-06-10 15:04:22 +1000

    uncomment code

--- some_code.R
+++ some_code.R
@@ -2,13 +2,13 @@
 ### Some Code ###
 #################
 
-# library(tidyverse)
-# library(ggplot2)
-# 
-# df <- mtcars %>%
-#   filter(cyl > 4)
-# 
-# ggplot(df,
-#        aes(x = mpg,
-#            y = hp)) +
-#   geom_point()
+library(tidyverse)
+library(ggplot2)
+
+df <- mtcars %>%
+  filter(cyl > 4)
+
+ggplot(df,
+       aes(x = mpg,
+           y = hp)) +
+  geom_point()
